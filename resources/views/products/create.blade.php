a15456edcdbfe0289246c3e76a058d31bded6384
MALWICK <batahumphrey66@gamil.com>
2023-12-19 12:58:40 +0100

    creating submit forms

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>this is the blade create product</h1>
     <form method="post">
-    <div>
-        <label for="name">Name:</label>
-        <input type="text" name="name" placeholder="Name"/>
-    </div>
-    <div>
-        <label for="name">Qty:</label>
-        <input type="" name="qty" placeholder="Qty"/>
-    </div>
-    <div>
-        <label for="name">Price:</label>
-        <input type="text" name="Price" placeholder="Price"/>
-    </div>
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" name="name" placeholder="Name" />
+        </div>
+        <div>
+            <label for="name">Qty:</label>
+            <input type="text" name="qty" placeholder="Qty" />
+        </div>
+        <div>
+            <label for="name">Price:</label>
+            <input type="text" name="Price" placeholder="Price" />
+        </div>
+        <div>
+            <label for="name">Description:</label>
+            <input type="Description" name="Description" placeholder="Description" />
+        </div>
     </form>
 </body>
+
 </html>
