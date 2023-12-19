eacca83376f18a088e4789bf0cbf15cc193a0668
MALWICK <batahumphrey66@gamil.com>
2023-12-19 12:57:00 +0100

    creating submit form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -7,5 +7,19 @@
 </head>
 <body>
     <h1>this is the blade create product</h1>
+    <form method="post">
+    <div>
+        <label for="name">Name:</label>
+        <input type="text" name="name" placeholder="Name"/>
+    </div>
+    <div>
+        <label for="name">Qty:</label>
+        <input type="" name="qty" placeholder="Qty"/>
+    </div>
+    <div>
+        <label for="name">Price:</label>
+        <input type="text" name="Price" placeholder="Price"/>
+    </div>
+    </form>
 </body>
 </html>
